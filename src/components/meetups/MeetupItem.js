453e9363e699558c3238be6455dd8243cd9fb121
Chris Sousa <chrisqcs@tuta.io>
2022-07-06 11:09:56 -0300

    MeetupItem with Card

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,23 @@
+import Card from "../ui/Card";
 import cx from "./MeetupItem.module.css";
 
 export default function MeetupItem(props) {
   const { image, title, address, description } = props;
   return (
     <li className={cx.item}>
-      <div className={cx.image}>
-        <img src={image} alt={title} />
-      </div>
-      <div className={cx.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={cx.actions}>
-        <button >Favorite</button>
-      </div>
+      <Card>
+        <div className={cx.image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={cx.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={cx.actions}>
+          <button>Favorite</button>
+        </div>
+      </Card>
     </li>
   );
 }

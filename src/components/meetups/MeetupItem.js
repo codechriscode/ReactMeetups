16d44d71b2d57b3ed55be92b708ac1461c70efe7
Chris Sousa <chrisqcs@tuta.io>
2022-07-05 23:11:25 -0300

    MeetupItem component

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,20 @@
+import cx from "./MeetupItem.module.css";
+
+export default function MeetupItem(props) {
+  const { image, title, address, description } = props;
+  return (
+    <li className={cx.item}>
+      <div className={cx.image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={cx.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+        <p>{description}</p>
+      </div>
+      <div className={cx.actions}>
+        <button >Favorite</button>
+      </div>
+    </li>
+  );
+}

eb63d260807ea6c0297c8e3b4da051c61a2bc7d1
Chris Sousa <chrisqcs@tuta.io>
2022-07-06 16:53:53 -0300

    Loading placeholder component

---
+++ src/components/ui/Loading.js
@@ -0,0 +1,15 @@
+import cx from "./Loading.module.css"
+
+export default function Loading() {
+  return (
+    <div class={cx.loading}>
+      <b>L</b>
+      <b>O</b>
+      <b>A</b>
+      <b>D</b>
+      <b>I</b>
+      <b>N</b>
+      <b>G</b>
+    </div>
+  );
+}

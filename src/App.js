4c2066c0205ad82423546200f53e6f0bc827a9b5
Chris Sousa <chrisqcs@tuta.io>
2022-07-05 22:36:50 -0300

    Routing setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import MainNav from "./components/layout/MainNav";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
 function App() {
-  return <div>Hello!</div>;
+  //localhost:3000
+  //meetmeup.com
+  return (
+    <>
+      <MainNav />
+      <Routes>
+        <Route element={<AllMeetups />} exact path="/" />
+        <Route element={<Favorites />} path="/favorites" />
+        <Route element={<NewMeetup />} path="/newMeetup" />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

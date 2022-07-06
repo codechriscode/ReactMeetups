b0e06f398994b3695b43ff09573de35800e59e40
Chris Sousa <chrisqcs@tuta.io>
2022-07-06 11:54:47 -0300

    NewMeetup page and mock update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -5,7 +5,7 @@ const DUMMY_DATA = [
     id: "m1",
     title: "This is a first meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Maresias_-_bresil.JPG/800px-Maresias_-_bresil.JPG",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
@@ -14,7 +14,7 @@ const DUMMY_DATA = [
     id: "m2",
     title: "This is a second meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://media-cdn.tripadvisor.com/media/photo-s/17/27/e7/7c/smoke-waterfall.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",

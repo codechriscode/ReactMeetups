f51179ca1aeb66d2ce710d4e122696509d9790e9
Chris Sousa <chrisqcs@tuta.io>
2022-07-13 16:30:48 -0300

    favorites-context

---
+++ src/store/favorites-context.js
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (favId) => {},
+  itemIsFavorite: (meetupId) => {},
+});
+
+export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup); //concat returns a new array
+    });
+  }
+
+  function removeFavoriteHandler(favId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((fav) => {
+        return fav.id !== favId; //filter returns a new array
+      });
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoritesContext;

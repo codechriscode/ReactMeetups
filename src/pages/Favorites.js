c4fb277a30b7b317f99b9d4436acb9809d2abb48
Chris Sousa <chrisqcs@tuta.io>
2022-07-13 17:30:38 -0300

    useContext(favs) in Favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,3 +1,18 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 export default function Favorites() {
-  return <div>Favorites component</div>
+  const favsCtx = useContext(FavoritesContext);
+
+  return (
+    <section>
+      <h1>Favorites</h1>
+      {favsCtx.totalFavorites ? (
+        <MeetupList list={favsCtx.favorites} />
+      ) : (
+        <h3>Add Favorites to see them here :D</h3>
+      )}
+    </section>
+  );
 }

dd76edcf6a3d549eac3862299e91568fe69b8edb
Chris Sousa <chrisqcs@tuta.io>
2022-07-13 17:50:12 -0300

    useContext(favs) in MainNav

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
-import cx from "./MainNav.module.css"
+import cx from "./MainNav.module.css";
+import FavoritesContext from "../../store/favorites-context";
+import { useContext } from "react";
 
 export default function MainNav() {
+  const favsCtx = useContext(FavoritesContext);
+
   return (
     <>
-      <header className={cx.header}><div className={cx.logo}>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">All Meetups</Link>
-          </li>
-          <li>
-            <Link to="/newMeetup">Add a Meetup</Link>
-          </li>
-          <li>
-            <Link to="/favorites">My Favorites</Link>
-          </li>
-        </ul>
-      </nav>
+      <header className={cx.header}>
+        <div className={cx.logo}>React Meetups</div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Meetups</Link>
+            </li>
+            <li>
+              <Link to="/newMeetup">Add a Meetup</Link>
+            </li>
+            <li>
+              <Link to="/favorites">My Favorites</Link>
+              <span className={cx.badge}>{favsCtx.totalFavorites}</span>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
